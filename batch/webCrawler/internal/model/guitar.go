de834f8be058499c6be4bc2404c2f6c16beec4f8
kazGear <kaz57guitar@gmail.com>
2026-05-29 00:28:38 +0900

    guitar struct 追加

--- batch/webCrawler/internal/model/guitar.go
+++ batch/webCrawler/internal/model/guitar.go
@@ -1,10 +1,23 @@
 package model
 
 type Guitar struct {
-	Model string
-	Maker string
-	Price int
-	URL   string
-	Image string
-	Shop  string
+	Maker         int     `json:"maker"`
+	Name          string  `json:"name"`
+	SubName       string  `json:"sub_name"`
+	BodyFinish    string  `json:"body_finish"`
+	BodyMaterial  int     `json:"body_material"`
+	Bridge        string  `json:"bridge"`
+	Color         string  `json:"color"`
+	Controls      string  `json:"controls"`
+	Comment       string  `json:"comment"`
+	Fingerboard   int     `json:"fingerboard"`
+	FretCount     int     `json:"fret_count"`
+	Inlays        string  `json:"inlays"`
+	Joint         string  `json:"joint"`
+	NeckMaterial  int     `json:"neck_material"`
+	Pickups       string  `json:"pickups"`
+	Price         int     `json:"price"`
+	ScaleLengthMM int     `json:"scale_length_mm"`
+	Series        string  `json:"series"`
+	Weight        float64 `json:"weight"`
 }
--- batch/webCrawler/internal/scraper/guitar.go
+++ batch/webCrawler/internal/scraper/guitar.go
@@ -3,7 +3,6 @@ package scraper
 import (
 	"github.com/gocolly/colly/v2"
 	"github.com/kazGear/portfolio/webCrawler/internal/model"
-	"github.com/kazGear/portfolio/webCrawler/pkg/utils"
 )
 
 func ScrapeGuitars() ([]model.Guitar, error) {
@@ -12,15 +11,15 @@ func ScrapeGuitars() ([]model.Guitar, error) {
     c := colly.NewCollector()
 
     c.OnHTML(".item", func(e *colly.HTMLElement) {
-        g := model.Guitar{
-            Model: e.ChildText(".model"),
-            Maker: e.ChildText(".maker"),
-            Price: utils.ParsePrice(e.ChildText(".price")),
-            URL:   e.ChildAttr("a", "href"),
-            Image: e.ChildAttr("img", "src"),
-            Shop:  "Ishibashi",
-        }
-        guitars = append(guitars, g)
+        // g := model.Guitar{
+        //     Model: e.ChildText(".model"),
+        //     Maker: e.ChildText(".maker"),
+        //     Price: utils.ParsePrice(e.ChildText(".price")),
+        //     URL:   e.ChildAttr("a", "href"),
+        //     Image: e.ChildAttr("img", "src"),
+        //     Shop:  "Ishibashi",
+        // }
+        // guitars = append(guitars, g)
     })
 
     err := c.Visit("https://kazapp-trial.com/")

466f643992f7d9ffbcb012e119a02cf53f06d859
kazGear <kaz57guitar@gmail.com>
2026-07-01 16:13:41 +0900

    TS DTO

---
+++ frontend/src/types/Code.ts
@@ -0,0 +1,5 @@
+export interface Code {
+    Category: string;
+    Code:     number;
+    Name:     string;
+};
---
+++ frontend/src/types/Guitar.ts
@@ -0,0 +1,34 @@
+export interface GuitarsResponse {
+    TotalCount: number;
+    Page:       number;
+    PageSize:   number;
+    TotalPages: number;
+    HasPrev:    boolean;
+    HasNext:    boolean;
+    Guitars:    Guitar[];
+}
+
+export interface Guitar {
+    Maker:            number;
+    Name:             string;
+    Color:            string;
+    ColorCd:          number;
+    BodyFinish:       string;
+    BodyMaterial:     string;
+    BodyMaterialTop:  number;
+    BodyMaterialBack: number;
+    Bridge:           string;
+    Controls:         string;
+    Comment:          string;
+    Fingerboard:      number;
+    FretCount:        number;
+    Inlays:           string;
+    Joint:            string;
+    NeckMaterial:     number;
+    Pickups:          string;
+    Price:            number;
+    ScaleLengthMm:    number;
+    Series:           string;
+    Src:              string;
+    Weight:           number;
+};

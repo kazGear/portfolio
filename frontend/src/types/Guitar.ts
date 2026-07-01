853755ea78646f4a24e829a0fd3bc42c17e8ab86
kazGear <kaz57guitar@gmail.com>
2026-07-01 16:20:30 +0900

    Update Guitar.ts

--- frontend/src/types/Guitar.ts
+++ frontend/src/types/Guitar.ts
@@ -1,3 +1,17 @@
+export interface GuitarsRequest {
+    MakerCd?:            number;
+    Name?:               string;
+    Series?:             string;
+    ColorCd?:            number;
+    BodyMaterialTopCd?:  number;
+    BodyMaterialBackCd?: number;
+    MinPrice?:           number;
+    MaxPrice?:           number;
+    Order?:               string;
+    Sort?:               string;
+    Page:                number;
+    PageSize:            number;
+};
 export interface GuitarsResponse {
     TotalCount: number;
     Page:       number;

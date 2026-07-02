26174af11f53027b5c2a4cbe82a60c0c70599517
kazGear <kaz57guitar@gmail.com>
2026-07-02 22:24:11 +0900

    検索結果INFO

--- frontend/src/components/GuitarGalleryPage/GuitarCards.tsx
+++ frontend/src/components/GuitarGalleryPage/GuitarCards.tsx
@@ -6,16 +6,22 @@ interface ArgProps {
     guitarsRes: GuitarsResponse | null;
 }
 
-const GuitarCards = ({guitarsRes: guitarsResponse}: ArgProps) => {
+const GuitarCards = ({guitarsRes: res}: ArgProps) => {
     return (
-        <div style={{display: "flex", flexWrap: "wrap", justifyContent: "space-evenly", margin: "15px"}}>
-            {
-                guitarsResponse?.guitars.map(guitar => (
-                    <GuitarCard guitar={guitar}
-                                key={guitar.maker + guitar.name + guitar.color} />
-                )
-            )}
-        </div>
+        <>
+            <p style={{marginLeft: "15px", fontWeight: "bolder"}}>
+                検索結果 {res?.totalCount} 件<br/>
+                ページ {res?.page} / {res?.totalPages} @{res?.pageSize}件
+            </p>
+            <div style={{display: "flex", flexWrap: "wrap", justifyContent: "space-evenly", margin: "15px"}}>
+                {
+                    res?.guitars.map(guitar => (
+                        <GuitarCard guitar={guitar}
+                                    key={guitar.maker + guitar.name + guitar.color} />
+                    )
+                )}
+            </div>
+        </>
     );
 }
 

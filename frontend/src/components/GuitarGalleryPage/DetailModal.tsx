ced783825f0104493861e772a78f02ce783d4dce
kazGear <kaz57guitar@gmail.com>
2026-07-03 16:51:34 +0900

    詳細モーダル 動確OK

--- frontend/src/components/GuitarGalleryPage/DetailModal.tsx
+++ frontend/src/components/GuitarGalleryPage/DetailModal.tsx
@@ -18,6 +18,8 @@ const Sbackground = styled.div`
 const Smodal = styled.div`
     width: 80%;
     height: 80%;
+    display: flex;
+    justify-content: space-evenly;
     border-radius: 20px;
     position: absolute;
     z-index: 2000;
@@ -37,12 +39,87 @@ interface ArgProps {
 }
 
 const DetailModal = ({selectedGuitars, isShow, callback}: ArgProps) => {
-    let isShowDetail = isShow ? "block" : "none"; // 詳細画面の表示制御
+    const isShowDetail = isShow ? "block" : "none"; // 詳細画面の表示制御
+    const guitar = selectedGuitars;
 
     return (
         <Sbackground style={{display: isShowDetail}}>
             <Smodal>
-                <Button text="閉じる" onClick={() => callback(false)}/>
+                <div style={{width: "50%", margin: "0px 40px"}}>
+                    <img src={guitar?.src} alt={guitar?.name} style={{width:"100%", height:"40%", objectFit: "contain", marginTop: "20px"}}/>
+                    <h2 style={{margin: "0px"}}>price:&emsp;{guitar?.price} 円</h2>
+                    <p>comment.</p>
+                    <p style={{overflowY: "scroll", fontSize: "14px", height: "30%"}}>{guitar?.comment}</p>
+                </div>
+                <div style={{width: "50%", margin: "0px 40px"}}>
+                    <h2>Guitars spec</h2>
+                    <table style={{overflowY: "scroll"}}>
+                        <tbody>
+                            <tr>
+                                <th>Name:&emsp;</th>
+                                <td>{guitar?.name}</td>
+                            </tr>
+                            <tr>
+                                <th>Color:&emsp;</th>
+                                <td>{guitar?.color}</td>
+                            </tr>
+                            <tr>
+                                <th>Series:&emsp;</th>
+                                <td>{guitar?.series}</td>
+                            </tr>
+                            <tr>
+                                <th>BodyMaterial:&emsp;</th>
+                                <td>{guitar?.bodyMaterial}</td>
+                            </tr>
+                            <tr>
+                                <th>BodyFinish:&emsp;</th>
+                                <td>{guitar?.bodyFinish}</td>
+                            </tr>
+                            <tr>
+                                <th>NeckMaterial:&emsp;</th>
+                                <td>{guitar?.neckMaterial}</td>
+                            </tr>
+                            <tr>
+                                <th>Fingerboard:&emsp;</th>
+                                <td>{guitar?.fingerboard}</td>
+                            </tr>
+                            <tr>
+                                <th>FretCount:&emsp;</th>
+                                <td>{guitar?.fretCount} frets</td>
+                            </tr>
+                            <tr>
+                                <th>Pickups:&emsp;</th>
+                                <td>{guitar?.pickups}</td>
+                            </tr>
+                            <tr>
+                                <th>Bridge:&emsp;</th>
+                                <td>{guitar?.bridge}</td>
+                            </tr>
+                            <tr>
+                                <th>Controls:&emsp;</th>
+                                <td>{guitar?.controls}</td>
+                            </tr>
+                            <tr>
+                                <th>Inlays:&emsp;</th>
+                                <td>{guitar?.inlays}</td>
+                            </tr>
+                            <tr>
+                                <th>Joint:&emsp;</th>
+                                <td>{guitar?.joint}</td>
+                            </tr>
+                            <tr>
+                                <th>ScaleLength:&emsp;</th>
+                                <td>{guitar?.scaleLengthMm} mm</td>
+                            </tr>
+                            <tr>
+                                <th>Weight:&emsp;</th>
+                                <td>{guitar?.weight} mm</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+                <Button text="閉じる" onClick={() => callback(false)} styleObj={{position: "absolute", right: "0", bottom: "0", margin: "0px 40px 40px 0px"}}/>
             </Smodal>
         </Sbackground>
     );

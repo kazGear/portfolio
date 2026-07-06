e35020725b1d028a3812b79bd17ec620d7b93b70
kazGear <kaz57guitar@gmail.com>
2026-07-06 11:10:16 +0900

    Update CommonLogic.ts

--- frontend/src/lib/CommonLogic.ts
+++ frontend/src/lib/CommonLogic.ts
@@ -13,3 +13,20 @@ export const isEmpty = (arg: any): boolean => {
 export const jsonClone = (arg: any) => {
     return JSON.parse(JSON.stringify(arg));
 }
+
+/** 年齢を算出、調整 (date: yyyy/mm/dd) */
+export const calcAge = (date: string): number => {
+    const birthDay = new Date(date);
+    const today = new Date();
+
+    const thisYearBirthDay = new Date(
+        today.getFullYear(),
+        birthDay.getMonth(),
+        birthDay.getDate()
+    );
+
+    let age = today.getFullYear() - birthDay.getFullYear();
+    if (today < thisYearBirthDay) age--;
+
+    return age;
+}

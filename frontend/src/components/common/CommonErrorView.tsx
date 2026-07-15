56bd1a855cd8df767c23cb29050ac6e01c7a3850
kazGear <kaz57guitar@gmail.com>
2026-07-15 23:10:47 +0900

    Update CommonErrorView.tsx

--- frontend/src/components/common/CommonErrorView.tsx
+++ frontend/src/components/common/CommonErrorView.tsx
@@ -1,20 +1,30 @@
 import styled from "styled-components";
 import ErrorImage from "../../images/background/error.png";
+import { SIZE } from "../../lib/Constants";
 
-const Body = styled.body`
+const Frame = styled.div`
     width: 100%;
+    height: calc(100vh - ${SIZE.HEADER_HEIGHT});
+    overflow-y: hidden;
+`;
+const Div = styled.div`
+    background: url(${ErrorImage});
+    background-size: contain;
     height: 100%;
+    overflow-y: hidden;
 `;
 const H1 = styled.h1`
-    margin: 40px;
+    margin: 40px 0px 0px 40px;
 `;
 
 const CommonErrorView = () => {
     return (
-        <Body style={{background: `url(${ErrorImage})`, backgroundSize: "contain"}}>
-            <H1>想定外のエラーが発生しました。</H1>
-            <H1>そんな日もありますよね(;^ω^)</H1>
-        </Body>
+        <Frame>
+            <Div>
+                <H1>想定外のエラーが発生しました。</H1>
+                <h3 style={{margin: "10px 0px 0px 40px"}}>そんな日もありますよね(;^ω^)</h3>
+            </Div>
+        </Frame>
     );
 }
 

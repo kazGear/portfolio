98a25f51885aaa03a486809edd3d258e7e6b583f
kazGear <kaz57guitar@gmail.com>
2026-07-22 09:31:13 +0900

    フォーマット

--- batch/webCrawler/internal/repository/sql/batchLogger.go
+++ batch/webCrawler/internal/repository/sql/batchLogger.go
@@ -22,12 +22,12 @@ func SelectBatchConfig() string {
 	return `
         SELECT
                exe.log_id                       AS log_id,
-			   exe.start_at                     AS start_at,
-			   config.batch_name                AS batch_name,
-			   config.scheduled_time            AS scheduled_time,
-			   config.start_delay_minutes       AS start_delay_minutes,
-			   config.expected_duration_minutes AS expected_duration_minutes,
-			   config.timeout_minutes           AS timeout_minutes
+               exe.start_at                     AS start_at,
+               config.batch_name                AS batch_name,
+               config.scheduled_time            AS scheduled_time,
+               config.start_delay_minutes       AS start_delay_minutes,
+               config.expected_duration_minutes AS expected_duration_minutes,
+               config.timeout_minutes           AS timeout_minutes
           FROM
                m_batch_config AS config
     INNER JOIN

24f0f104cf9590a09a6d5d2ce5fbb835dee089fc
kazGear <kaz57guitar@gmail.com>
2026-07-28 13:11:14 +0900

    zoom test

--- frontend/src/components/GuitarGalleryPage/DetailModal.tsx
+++ frontend/src/components/GuitarGalleryPage/DetailModal.tsx
@@ -5,6 +5,8 @@ import CommonButton from "../common/CommonButton";
 import DetailLeftSide from "./DetailLeftSide";
 import DetailRightSide from "./DetailRightSide";
 
+console.log("DetailLeftSide =", DetailLeftSide);
+
 const Background = styled.div`
     width: 100%;
     height: 100%;

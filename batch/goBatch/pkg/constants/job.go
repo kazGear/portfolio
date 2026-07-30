356149db6f53046a116c81fcf1a31b3a5377e768
kazGear <kaz57guitar@gmail.com>
2026-07-30 17:38:24 +0900

    Update job.go

--- batch/goBatch/pkg/constants/job.go
+++ batch/goBatch/pkg/constants/job.go
@@ -1,22 +1,21 @@
 package constants
 
-// Guitar Spec Keys
 const (
-    Title = "Title"
-	Url = "Url"
+	Title       = "Title"
+	Url         = "Url"
 	Description = "Description"
 )
 
 const (
-    Language         = "LANGUAGE"
-    FrameworkLibrary = "FRAMEWORK_LIBRARY"
-    Database         = "DATABASE"
-    Cloud            = "CLOUD"
-    Infrastructure   = "INFRASTRUCTURE"
-    Tool             = "TOOL"
-    Test             = "TEST"
-    Architecture     = "ARCHITECTURE"
-    Methodology      = "METHODOLOGY"
+	Language         = "LANGUAGE"
+	FrameworkLibrary = "FRAMEWORK_LIBRARY"
+	Database         = "DATABASE"
+	Cloud            = "CLOUD"
+	Infrastructure   = "INFRASTRUCTURE"
+	Tool             = "TOOL"
+	Test             = "TEST"
+	Architecture     = "ARCHITECTURE"
+	Methodology      = "METHODOLOGY"
 	Role             = "ROLE"
-    Other            = "OTHER"
+	Other            = "OTHER"
 )

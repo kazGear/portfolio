ec4d2a0ae75d3770d83c2d3871b62c2281b11aba
kazGear <kaz57guitar@gmail.com>
2026-08-06 10:12:17 +0900

    Update db.go

--- batch/goBatch/pkg/db/db.go
+++ batch/goBatch/pkg/db/db.go
@@ -9,14 +9,34 @@ import (
 )
 
 func Connect() *sqlx.DB {
+	dbHost     := os.Getenv("DB_HOST")
+	dbPort     := os.Getenv("DB_PORT")
+	dbUser     := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName 	   := os.Getenv("DB_NAME")
+
     dsn := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		dbHost,
+		dbPort,
+		dbUser,
+		dbPassword,
+		dbName,
 	)
-    db := sqlx.MustConnect("postgres", dsn)
+    db, err := sqlx.Connect("postgres", dsn)
+
+	if err == nil {
+		return db
+	} else {
+		dsn = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			os.Getenv("DB_SERVICE_NAME"),
+			dbPort,
+			dbUser,
+			dbPassword,
+			dbName,
+		)
+		db = sqlx.MustConnect("postgres", dsn)
+	}
     return db
 }
